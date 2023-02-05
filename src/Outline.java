17c8a8ecdae5880b87b6a8309f9585dd0530f039
Charhmwang <charwanghm@gmail.com>
2023-02-05 13:11:00 +0000

    Solution 1.1-1.4

--- src/Outline.java
+++ src/Outline.java
@@ -1,9 +1,74 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1,7,3,4,8,2};
-    System.out.println(Arrays.asList(intArray));
-    // Arrays.sort(intArray,.......)
-  } 
+
+  public static String[] getWords() {
+//    return new String[] {
+//            "software design programming",
+//            "computing systems",
+//            "information systems",
+//            "programming in java",
+//            "data knowledge management",
+//            "cloud computing"
+//    };
+    return new String[] {
+            "fly",
+            "ants",
+            "bats",
+            "piglets",
+            "gorillas",
+            "cat",
+            "hamster",
+            "emus",
+            "tiger" };
+  }
+
+  // Create a words array
+  // Sort the array by length (i.e., shortest to longest)
+  public static void question_1_1() {
+    String[] words = getWords();
+    Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
+    // Alternatively
+    // Arrays.sort(words, Comparator.comparingInt(String::length));
+    System.out.println("1.1: " + Arrays.asList(words));
+  }
+
+  // Sort the array by reverse length (i.e., longest to shortest)
+  public static void question_1_2() {
+    String[] words = getWords();
+    Arrays.sort(words, Comparator.comparingInt(String::length).reversed());
+    // Alternatively
+    // Arrays.sort(words, (a, b) -> Integer.compare(b.length(), a.length()));
+    System.out.println("1.1: " + Arrays.asList(words));
+  }
+
+  // Sort the array by comparing first character
+  public static void question_1_3() {
+    String[] words = getWords();
+    Arrays.sort(words, Comparator.comparingInt(a -> a.charAt(0)));
+    // Alternatively
+    // Arrays.sort(words, (a, b) -> Integer.compare(a.charAt(0), b.charAt(0)));
+  }
+
+  // Sort the array by Strings that contain `"e"` first, everything else second.
+  public static void question_1_4() {
+    String[] words = getWords();
+    Arrays.sort(words, (a, b) -> {
+      if ( a.contains("e") && !(b.contains("e")) ) return -1;
+      if ( b.contains("e") && !(a.contains("e")) ) return 1;
+      return 0;
+    });
+    System.out.println("1.4:" + Arrays.asList(words));
+  }
+
+
+
+  public static void main(String...args) {
+
+      question_1_1();
+      question_1_2();
+      question_1_3();
+      question_1_4();
+  }
 }
